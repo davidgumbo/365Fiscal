cc5421fb921eb8f1919a4438f74fea088ef00457
Coconut <Coconut9021@gmail.com>
2026-02-23 11:16:32 +0200

    adjusted the icons to sue lucide icon import

--- frontend/src/pages/InventoryPage.tsx
+++ frontend/src/pages/InventoryPage.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { apiFetch } from "../api";
 import { useMe } from "../hooks/useMe";
 import { useCompanies, Company } from "../hooks/useCompanies";
+import { Trash2 } from "lucide-react";
 
 // ============= TYPES =============
 type Category = {
@@ -249,26 +250,9 @@ const OperationsIcon = ({ color }: InventoryIconProps) => (
   </svg>
 );
 
-const TrashIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    class="lucide lucide-trash2-icon lucide-trash-2"
-  >
-    <path d="M10 11v6" />
-    <path d="M14 11v6" />
-    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
-    <path d="M3 6h18" />
-    <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
-  </svg>
-);
+const TrashIcon = () => {
+  return <Trash2 />;
+};
 
 const EditIcon = () => (
   <svg
